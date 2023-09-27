b544d5fbbcb455e231a1ea82e471eea8cbc88ad3
PallaviKatari <pallavikatari@gmail.com>
2023-09-27 15:35:45 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using Core_API_APIKey.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+//Pass ApiKey whish is givewn in appsettings.json in Authorization - Postman Tool
+app.UseMiddleware<ApiKeyMiddleware>();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
